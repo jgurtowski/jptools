9d42bc4dc958600e1ad9661686eaa30bff3250f7
James Gurtowski <gurtowsk@cshl.edu>
2015-11-04 16:40:49 -0500

    updated fast5tofasta

--- jptools/fast5.py
+++ jptools/fast5.py
@@ -4,7 +4,9 @@ import logging
 import sys
 from operator import itemgetter
 from functools import partial
-from itertools import imap, izip, ifilter
+from itertools import imap, izip, ifilter, chain
+
+from jbio.fastq import record_iterator as fastq_iterator
 
 def fast5ToFasta_main():
     if not len(sys.argv) >= 2:
@@ -21,17 +23,6 @@ def fast5ToFasta_main():
 def fast5ToFasta(f5files, outfh, logfh):
     '''Takes fast5files and outputs to output_file and log_file'''
     opj = os.path.join
-    
-    def fqstrToArr(fqstr):
-        arr = fqstr.strip().split()
-        if not len(arr) == 4:
-            logfh("Fastq string looks corrupt %s \n" % arr[0])
-            return None
-        tname = arr[0][1:]
-        arr[0] = tname
-        tdesc = arr[2][1:]
-        arr[2] = tdesc
-        return arr
 
     for f5file in f5files:
         logfh.write("Fast5File %s\n" % f5file)
@@ -51,18 +42,16 @@ def fast5ToFasta(f5files, outfh, logfh):
                 logfh.write("Found %d BaseCall Groups : %s\n" % (len(bcg_suffix), ",".join(bcg_suffix)))
         
                 fq_grp_names = map( lambda x : opj(opj(basecall_root, x), "Fastq"), basecalled_grps)
-        
-                fq_datasets =  map(lambda x : f5fh.get(x), fq_grp_names)
-        
-                fq_arrs = imap(lambda x : fqstrToArr(x[()]), fq_datasets)
-        
-                #(bcg_suffix, [fastq_arr])
-                filt_arrs = ifilter(lambda x : not itemgetter(1)(x) == None,  izip(bcg_suffix, fq_arrs))
-                fastas = imap(lambda x : (x[1][0]+"_"+exp_start+"_"+x[0], x[1][1]), filt_arrs)
-                    
-                for fasta in fastas:
-                    outfh.write( "\n".join([">"+fasta[0], fasta[1]]) )
-                    outfh.write( "\n")
+
+                fq_datasets =  map(lambda y : f5fh.get(y), fq_grp_names)
+
+                fastq_arrays = imap(lambda d: d[()].strip().split("\n"), fq_datasets)
+                
+                fastq_objs = fastq_iterator( chain.from_iterable(fastq_arrays))
+                
+                for fastq_obj in fastq_objs:
+                    outfh.write( ">" + fastq_obj.name + "\n")
+                    outfh.write( fastq_obj.seq + "\n")
         except Exception as e:
             logfh.write("Error : %s" % str(e))
             pass
